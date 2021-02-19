37f02f7fc4280c4fce1b90e10e6050c054d268d3
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2021-02-19 15:34:33 -0500

    Create computer_storage.py

---
+++ computer storage/computer_storage.py
@@ -0,0 +1,10 @@
+import shutil
+
+total_b, used_b, free_b = shutil.disk_usage('.')
+
+gb = 10 ** 9
+
+print('Total: {:6.2f} GB'.format(total_b / gb))
+print('Used : {:6.2f} GB'.format(used_b / gb))
+print('Free : {:6.2f} GB'.format(free_b / gb))
+
